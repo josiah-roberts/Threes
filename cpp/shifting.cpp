9460bb437f156c203268f41e791056f5899d3ce5
joflashstudios <joflashstudios@gmail.com>
2018-09-25 21:15:56 -0700

    Some shifting cleanup

--- cpp/shifting.cpp
+++ cpp/shifting.cpp
@@ -6,57 +6,49 @@ bool combine(int a, int b, int *result)
     return (a == b && a > 2) || *result == 3 || (a == 0 ^ b == 0);
 }
 
+bool shift(int (&array)[16], int sourceCell, int targetCell)
+{
+    int combination;
+    int a = array[targetCell];
+    int b = array[sourceCell];
+
+    if (combine(a, b, &combination))
+    {
+        array[targetCell] = combination;
+        array[sourceCell] = 0;
+        return true;
+    }
+
+    return false;
+}
+
 bool can_shift_left(int (&array)[16])
 {
-    bool moved = false;
-
     for (int row = 0; row < 4; row++)
     for (int column = 1; column < 4; column++)
-    {
-        int combination;
-        int a = array[row * 4 + (column - 1)];
-        int b = array[row * 4 + column];
-
-        if (combine(a, b, &combination))
-            return true;
-    }
+    if (shift(array, row * 4 + column, row * 4 + (column - 1)))
+        return true;
 
     return false;
 }
 
 bool can_shift_right(int (&array)[16])
 {
-    bool moved = false;
-
     for (int row = 0; row < 4; row++)
     for (int column = 2; column >= 0; column--)
-    {
-        int combination;
-        int a = array[row * 4 + (column + 1)];
-        int b = array[row * 4 + column];
-
-        if (combine(a, b, &combination))
-            return true;
-    }
+    if (shift(array, row * 4 + column, row * 4 + (column + 1)))
+        return true;
 
     return false;
 }
 
 bool can_shift_up(int (&array)[16])
 {
-    bool moved = false;
-
     for (int row = 1; row < 4; row++)
     for (int column = 0; column < 4; column++)
-    {
-        int combination;
-        int a = array[(row - 1) * 4 + column];
-        int b = array[row * 4 + column];
-
-        if (combine(a, b, &combination))
-            return true;
-    }
-
+    if (shift(array, row * 4 + column, (row - 1) * 4 + column))
+        return true;
+            
     return false;
 }
 
@@ -66,14 +58,8 @@ bool can_shift_down(int (&array)[16])
 
     for (int row = 2; row >= 0; row--)
     for (int column = 0; column < 4; column++)
-    {
-        int combination;
-        int a = array[(row + 1) * 4 + column];
-        int b = array[row * 4 + column];
-
-        if (combine(a, b, &combination))
-            return true;
-    }
+    if (shift(array, row * 4 + column, (row + 1) * 4 + column))
+        return true;
 
     return false;
 }
@@ -84,18 +70,8 @@ bool shift_left(int (&array)[16])
 
     for (int row = 0; row < 4; row++)
     for (int column = 1; column < 4; column++)
-    {
-        int combination;
-        int a = array[row * 4 + (column - 1)];
-        int b = array[row * 4 + column];
-
-        if (combine(a, b, &combination))
-        {
-            array[row * 4 + (column - 1)] = combination;
-            array[row * 4 + column] = 0;
-            moved = true;
-        }
-    }
+    if (shift(array, row * 4 + column, row * 4 + (column - 1)))
+        moved = true;
 
     return moved;
 }
@@ -106,18 +82,8 @@ bool shift_right(int (&array)[16])
 
     for (int row = 0; row < 4; row++)
     for (int column = 2; column >= 0; column--)
-    {
-        int combination;
-        int a = array[row * 4 + (column + 1)];
-        int b = array[row * 4 + column];
-
-        if (combine(a, b, &combination))
-        {
-            array[row * 4 + (column + 1)] = combination;
-            array[row * 4 + column] = 0;
-            moved = true;
-        }
-    }
+    if (shift(array, row * 4 + column, row * 4 + (column + 1)))
+        moved = true;
 
     return moved;
 }
@@ -128,18 +94,8 @@ bool shift_up(int (&array)[16])
 
     for (int row = 1; row < 4; row++)
     for (int column = 0; column < 4; column++)
-    {
-        int combination;
-        int a = array[(row - 1) * 4 + column];
-        int b = array[row * 4 + column];
-
-        if (combine(a, b, &combination))
-        {
-            array[(row - 1) * 4 + column] = combination;
-            array[row * 4 + column] = 0;
-            moved = true;
-        }
-    }
+    if (shift(array, row * 4 + column, (row - 1) * 4 + column))
+        moved = true;
 
     return moved;
 }
@@ -150,18 +106,8 @@ bool shift_down(int (&array)[16])
 
     for (int row = 2; row >= 0; row--)
     for (int column = 0; column < 4; column++)
-    {
-        int combination;
-        int a = array[(row + 1) * 4 + column];
-        int b = array[row * 4 + column];
-
-        if (combine(a, b, &combination))
-        {
-            array[(row + 1) * 4 + column] = combination;
-            array[row * 4 + column] = 0;
-            moved = true;
-        }
-    }
+    if (shift(array, row * 4 + column, (row + 1) * 4 + column))
+        moved = true;
 
     return moved;
 }
