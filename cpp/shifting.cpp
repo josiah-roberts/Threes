58c5a7d06a4a6fca3e7c7a43c84d2c879543f72c
joflashstudios <joflashstudios@gmail.com>
2018-09-25 21:43:03 -0700

    Made non-mutating functions not mutate state

--- cpp/shifting.cpp
+++ cpp/shifting.cpp
@@ -1,9 +1,14 @@
 #include "shifting.h"
 
+bool can_combine(int a, int b)
+{
+    return (a == b && a > 2) || a + b == 3 || (a == 0 ^ b == 0);
+}
+
 bool combine(int a, int b, int *result)
 {
     *result = a + b;
-    return (a == b && a > 2) || *result == 3 || (a == 0 ^ b == 0);
+    return can_combine(a, b);
 }
 
 bool shift(int (&array)[16], int sourceCell, int targetCell)
@@ -26,7 +31,7 @@ bool can_shift_left(int (&array)[16])
 {
     for (int row = 0; row < 4; row++)
     for (int column = 1; column < 4; column++)
-    if (shift(array, row * 4 + column, row * 4 + (column - 1)))
+    if (can_combine(array[row * 4 + column], array[row * 4 + (column - 1)]))
         return true;
 
     return false;
@@ -36,7 +41,7 @@ bool can_shift_right(int (&array)[16])
 {
     for (int row = 0; row < 4; row++)
     for (int column = 2; column >= 0; column--)
-    if (shift(array, row * 4 + column, row * 4 + (column + 1)))
+    if (can_combine(array[row * 4 + column], array[row * 4 + (column + 1)]))
         return true;
 
     return false;
@@ -46,7 +51,7 @@ bool can_shift_up(int (&array)[16])
 {
     for (int row = 1; row < 4; row++)
     for (int column = 0; column < 4; column++)
-    if (shift(array, row * 4 + column, (row - 1) * 4 + column))
+    if (can_combine(array[row * 4 + column], array[(row - 1) * 4 + column]))
         return true;
             
     return false;
@@ -58,7 +63,7 @@ bool can_shift_down(int (&array)[16])
 
     for (int row = 2; row >= 0; row--)
     for (int column = 0; column < 4; column++)
-    if (shift(array, row * 4 + column, (row + 1) * 4 + column))
+    if (can_combine(array[row * 4 + column], array[(row + 1) * 4 + column]))
         return true;
 
     return false;
